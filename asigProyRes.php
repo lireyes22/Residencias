e0fb074b26de8e94840ee0bea2abfc391bd7c444
CesarXiu <cesarxiu504@gmail.com>
2023-04-27 23:54:35 -0500

    Nuevos botones

--- asigProyRes.php
+++ asigProyRes.php
@@ -23,8 +23,10 @@
 			</div>
 		</div>
 		<div class="button-row">
-			<a href="a.html" class="button-link">Asignación de Solicitud de Proyecto</a>
-			<a href="asigProyRes.php" class="button-link">Asignación de Solicitud de Residencia</a>
+			<a href="a.html" class="button-link">Profesores</a>
+			<a href="a.html" class="button-link">Solicitudes de Residencias</a>
+			<a href="asigProyRes.php" class="button-link">Solicitudes de Proyectos</a>
+			<a href="a.html" class="button-link">Lista proyectos</a>
 		</div>
 	</div>
 	<div class="tabla-scroll">
