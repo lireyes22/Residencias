6cb3f6a16de65fac09d51103311e9bf693edd07e
Nefta56 <128199555+Nefta56@users.noreply.github.com>
2023-04-30 22:16:54 -0500

    s

---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Residencias</title>
+	<link rel="stylesheet" href="style/style.css"> 
+	<link rel="stylesheet" href="style/styleLogin.css">
+	<meta charset="utf-8">
+</head>
+
+<body style="margin: 0;">
+	<div class="page-login">
+		<div class="loginCentral">
+			<div class="box1">
+				<h1>Login</h1>
+			</div>
+			<div class="box2">
+				<form>							
+					<input type="text" id="username" name="username" placeholder="Usuario">
+					<input type="password" id="password" name="password" placeholder="Password">
+					<div class="combo">
+						<select class="combo-select">
+							<option value="option1">Alumno</option>
+							<option value="option2">Profesor</option>
+							<option value="option3">Trabajador</option>
+						</select>
+					</div>
+					<button type="submit">Login</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
