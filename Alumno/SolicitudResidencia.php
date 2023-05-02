1f3b6a10573290306eb8b2bcbbf8f51714720b83
Daniel Hernández Palomar <dh072878@gmail.com>
2023-05-02 18:24:48 -0500

    Update SolicitudResidencia.php

--- Alumno/SolicitudResidencia.php
+++ Alumno/SolicitudResidencia.php
@@ -33,6 +33,7 @@
 	</div>
     <br>
     <!----------------------------------------------------- Fieldset Proyecto ---------------------------------------------------------->
+    <form action="">
     <fieldset class="bg-fldst">
     <legend class="legend">Proyecto</legend>
     <article>
@@ -40,23 +41,23 @@
             <div class="columnaL">
                 <div class="form-row">
                     <label for="jefeDiv">Jefe(a) de la Div. de Estudios Profesionales:</label>
-                    <input type="text" id="jefeDiv" name="jefeDiv" value="<?php echo 'Juan Cocio' ?>" disabled='disabled'>
+                    <input type="text" id="jefeDiv" name="jefeDiv" value="<?php echo 'Juan Cocio' ?>" disabled='disabled' required>
                 </div>
                 <div class="form-row">
                     <label for='coordinador'>Coord. de la Carrera de:</label>
-                    <input type="text" id="coordinador" name="coordinador" value="<?php echo 'Benja' ?>" disabled='disabled'>
+                    <input type="text" id="coordinador" name="coordinador" value="<?php echo 'Benja' ?>" disabled='disabled' required>
                 </div>
                 <div class="form-row">
                     <label for="">AT'N C:</label>
-                    <input type="text" id="" name="" value="<?php echo '' ?>">
+                    <input type="text" id="" name="" value="<?php echo '' ?>" required>
                 </div>
                 <div class="form-row">
                     <label for="fecha">Fecha:</label>
-                    <input type="date" id="fecha" name="fecha">
+                    <input type="date" id="fecha" name="fecha" required>
                 </div>
                 <div class="form-row">
                     <label for="NombreProyecto">Nombre del Proyecto:</label>
-                    <input type="text" id="NombreProyecto" name="NombreProyecto" value="<?php echo 'Inteligencia Artificial' ?>" disabled='disabled'>
+                    <input type="text" id="NombreProyecto" name="NombreProyecto" value="<?php echo 'Inteligencia Artificial' ?>" disabled='disabled' required>
                 </div>
             </div>
             <div class="columnaR">
@@ -79,15 +80,15 @@
                 </div>
                 <div class="form-row">
                     <label for="peridoProyect">Periodo proyectado:</label>
-                    <input type="text" name="peridoProyect" id = "peridoProyect">
+                    <input type="text" name="peridoProyect" id = "peridoProyect" required>
                 </div>
                 <div class="form-row">
                     <label for="nomAsesorInterno">Nombre Asesor Interno:</label>
-                    <input type="text" id="nomAsesorInterno" name="nomAsesorInterno" value="<?php echo 'Blandy' ?>" disabled='disabled'>
+                    <input type="text" id="nomAsesorInterno" name="nomAsesorInterno" value="<?php echo 'Blandy' ?>" disabled='disabled' required>
                 </div>
                 <div class="form-row">
                     <label for="SPVacantes">Numero Residentes:</label>
-                    <input type="number" name="SPVacantes" id="SPVacantes" min="1" max="4" placeholder="0">
+                    <input type="number" name="SPVacantes" id="SPVacantes" min="1" max="4" placeholder="0" required>
                 </div>
             </div>
             </section>
@@ -102,7 +103,7 @@
                 <div class="columnaL">
                     <div class="form-row">
                     <label for="ENombre">Nombre:</label>
-                    <input type="text" id="ENombre" name="ENombre" value="<?php echo 'Bon Ice' ?>" disabled='disabled'>
+                    <input type="text" id="ENombre" name="ENombre" value="<?php echo 'Bon Ice' ?>" disabled='disabled' required>
                     </div>
                     <div class="form-row">
                         <label for="Eramo">Ramo:</label>
@@ -114,7 +115,7 @@
                     </div>
                     <div class="form-row">
                         <label for="ERFC">RFC:</label>
-                        <input type="text" id="ERFC" name="ERFC" value="<?php echo 'VECJ880326' ?>" disabled='disabled'>
+                        <input type="text" id="ERFC" name="ERFC" value="<?php echo 'VECJ880326' ?>" disabled='disabled' required>
                     </div>
                     <div class="form-row">
                         <label for="ESector">Sector:</label>
@@ -125,65 +126,65 @@
                     </div>
                     <div class="form-row">
                         <label for="EActPrincipal">Actividad principal de la empresa:</label>
-                        <input type="text" id="EActPrincipal" name="EActPrincipal" value="<?php echo 'Venta de Saborines' ?>" disabled='disabled'>
+                        <input type="text" id="EActPrincipal" name="EActPrincipal" value="<?php echo 'Venta de Saborines' ?>" disabled='disabled' required>
                     </div>
                     <div class="form-row">
                         <label for="EDomicilio">Domicilio:</label>
-                        <input type="text" id="EDomicilio" name="EDomicilio" value="<?php echo 'C.robles #354 ' ?>" disabled='disabled'>  
+                        <input type="text" id="EDomicilio" name="EDomicilio" value="<?php echo 'C.robles #354 ' ?>" disabled='disabled'>  required 
                     </div>
                 </div>
 
                 <div class="columnaC">
                     <div class="form-row">
                         <label for="EColonia">Colonia:</label>
-                        <input type="text" id="EColonia" name="EColonia" value="<?php echo 'Forjadores' ?>" disabled='disabled'>
+                        <input type="text" id="EColonia" name="EColonia" value="<?php echo 'Forjadores' ?>" disabled='disabled' required>
                     </div>
                     <div class="form-row">
                         <label for="ECp">CP:</label>
-                        <input type="text" id="ECp" name="ECp" value="<?php echo '77000' ?>" disabled='disabled'>
+                        <input type="text" id="ECp" name="ECp" value="<?php echo '77000' ?>" disabled='disabled' required>
                     </div>
                     <div class="form-row">
                         <label for="EFax">FAX:</label>
-                        <input type="text" id="EFax" name="EFax" value="<?php echo '484464848' ?>" disabled='disabled'>
+                        <input type="text" id="EFax" name="EFax" value="<?php echo '484464848' ?>" disabled='disabled' required>
                     </div>
                     <div class="form-row">
                         <label for="ECiudad">Ciudad:</label>
-                        <input type="text" id="ECiudad" name="ECiudad" value="<?php echo 'Chetumal' ?>" disabled='disabled'>
+                        <input type="text" id="ECiudad" name="ECiudad" value="<?php echo 'Chetumal' ?>" disabled='disabled' required>
                     </div>
                     <div class="form-row">
                         <label for="ETelefono">Teléfono:</label>
-                        <input type="tel" id="ETelefono" name="ETelefono" value="<?php echo '983-445-6778' ?>">
+                        <input type="tel" id="ETelefono" name="ETelefono" value="<?php echo '983-445-6778' ?>" required>
                     </div>
                     <div class="form-row">
                         <label for="ETelefonoDos">Segundo Telefono:</label>
-                        <input type="tel" id="ETelefonoDos" name="ETelefonoDos" placeholder="983-445-6778">
+                        <input type="tel" id="ETelefonoDos" name="ETelefonoDos" placeholder="983-445-6778" required>
                     </div>
                 </div>
                 
                 <div class="columnaR">
                     <div class="form-row">
                         <label for="nombreTitular">Nombre del titular de la empresa:</label>
-                        <input type="text" id="nombreTitular" name="nombreTitular">
+                        <input type="text" id="nombreTitular" name="nombreTitular" required>
                     </div>
                     <div class="form-row">
                         <label for="puestoTitular">Puesto:</label>
-                        <input type="text" name="puestoTitular" id="puestoTitular">
+                        <input type="text" name="puestoTitular" id="puestoTitular" required>
                     </div>
                     <div class="form-row">
                         <label for="nomAsesorExterno">Nombre del Asesor externo:</label>
-                        <input type="text" name="nomAsesorExterno" id="nomAsesorExterno" value="<?php echo 'Juan Perez'?>" disabled='disabled'>
+                        <input type="text" name="nomAsesorExterno" id="nomAsesorExterno" value="<?php echo 'Juan Perez'?>" disabled='disabled' required>
                     </div>
                     <div class="form-row">
                         <label for="puestoAsesor">Puesto:</label>
-                        <input type="text" name="puestoAsesor" id="puestoAsesor">
+                        <input type="text" name="puestoAsesor" id="puestoAsesor" required>
                     </div>
                     <div class="form-row">
                         <label for="ENombreEncargado">Nombre de la persona que firmará el acuerdo de trabajo. Estudiante- Escuela-Empresa:</label>
-                        <input type="text" name="ENombreEncargado" id="ENombreEncargado">
+                        <input type="text" name="ENombreEncargado" id="ENombreEncargado" required>
                     </div>
                     <div class="form-row">
                         <label for="puestoEncargado">Puesto:</label>
-                        <input type="text" name="puestoEncargado" id="puestoEncargado">
+                        <input type="text" name="puestoEncargado" id="puestoEncargado" required>
                     </div>                      
                 </div>
             </section>
@@ -198,64 +199,65 @@
             <div class="columnaL">
                 <div class="form-row">
                     <label for="nombreResidente">Nombre:</label>
-                    <input type="text" id="nombreResidente" name="nombreResidente" value="<?php echo 'Maria De Todos los Angeles' ?>" disabled='disabled'>
+                    <input type="text" id="nombreResidente" name="nombreResidente" value="<?php echo 'Maria De Todos los Angeles' ?>" disabled='disabled' required>
                 </div>
                 <div class="form-row">
                     <label for="carrera">Carrera:</label>
-                    <input type="text" id="carrera" name="carrera" value="<?php echo 'Ing. en Sistemas' ?>" disabled='disabled'>
+                    <input type="text" id="carrera" name="carrera" value="<?php echo 'Ing. en Sistemas' ?>" disabled='disabled' required>
                 </div>
                 <div class="form-row">
                     <label for="numControl">Numero de Control:</label>
-                    <input type="text" id="numControl" name="numControl" value="<?php echo '20152255' ?>" disabled='disabled'>
+                    <input type="text" id="numControl" name="numControl" value="<?php echo '20152255' ?>" disabled='disabled' required>
                 </div>
                 <div class="form-row">
                     <label for="numSemestre">Semestre a cursar:</label>
-                    <input type="number" id="numSemestre" name="numSemestre" min="8" max="15" value="8">
+                    <input type="number" id="numSemestre" name="numSemestre" min="8" max="15" value="8" required>
                 </div>
                 <div class="form-row">
                     <label for="domicilio">Domicilio:</label>
-                    <input type="text" id="domicilio" name="domicilio" value="<?php echo '#254' ?>" disabled='disabled'>
+                    <input type="text" id="domicilio" name="domicilio" value="<?php echo '#254' ?>" disabled='disabled' required>
                 </div>
                 <div class="form-row">
                     <label for="email">Email:</label>
-                    <input type="email" id="email" name="email" value="<?php echo 'l20152255@chetumal.tecnm.mx' ?>" disabled='disabled'>
+                    <input type="email" id="email" name="email" value="<?php echo 'l20152255@chetumal.tecnm.mx' ?>" disabled='disabled' required>
                 </div>
             </div>
 
             <div class="columnaR">
                 <div class="form-row">
                     <label for="numeroSeguro">Para seguridad social acudir:</label>
-                    <input type="text" name="numeroSeguro" placeholder="Numero">
+                    <input type="text" name="numeroSeguro" placeholder="Numero" required>
                 </div>
                 <div class="form-row">
-                    <input type="radio" id="imss" name="tipoSeguro" value="IMSS">
+                    <input type="radio" id="imss" name="tipoSeguro" value="IMSS" required>
                     <label for="imss">IMSS</label>
-                    <input type="radio" id="issste" name="tipoSeguro" value="ISSSTE">
+                    <input type="radio" id="issste" name="tipoSeguro" value="ISSSTE" required>
                     <label for="issste">ISSSTE</label>
-                    <input type="radio" id="otro" name="tipoSeguro" value="OTROS">
+                    <input type="radio" id="otro" name="tipoSeguro" value="OTROS" required>
                     <label for="otro">OTROS</label>
                 </div>
                 <div class="form-row">
                     <label for="cuidad">Ciudad:</label>
-                    <input type="text" id="cuidad" name="cuidad" value="<?php echo 'Chetumal' ?>" disabled ='disabled'>
+                    <input type="text" id="cuidad" name="cuidad" value="<?php echo 'Chetumal' ?>" disabled ='disabled' required>
                 </div>
                 <div class="form-row">
                     <label for="telefono">Teléfono:</label>
-                    <input type="tel" name="telAlumno" id="telefono" placeholder="983-445-6778">
+                    <input type="tel" name="telAlumno" id="telefono" placeholder="983-445-6778" required>
                 </div>
                 <div id="firma" class="form-row">
                     <div>
                         <label for="firma">Firma del Estudiante: </label>
-                        <input type="file" id="firma" name="firma">
+                        <input type="file" id="firma" name="firma" required>
                     </div>
                 </div>
                 <div id="antepro" class="form-row">
                     <div>
                         <label for="SRAnteProyecto">Anteproyecto: </label>
-                        <input type="file" id="SRAnteProyecto" name="SRAnteProyecto">
+                        <input type="file" id="SRAnteProyecto" name="SRAnteProyecto" required>
                     </div>
                 </div>                  
             </div>
+            </form>
             <div class="caja tb-th-asp">
                 <input type="submit" value="Enviar Solicitud">
             </div>
