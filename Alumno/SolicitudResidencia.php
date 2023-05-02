dc260c41a70fa22f955b2e4803c93dbf2efd2538
Daniel Hernández Palomar <dh072878@gmail.com>
2023-05-02 18:08:59 -0500

    aasa
    erwewe

--- Alumno/SolicitudResidencia.php
+++ Alumno/SolicitudResidencia.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
 <html>
+    
 <head>
-    <link rel="stylesheet" type="text/css" href="../style/styleSolicRes.css" title="styleSolicRes">
+    <link rel="stylesheet" type="text/css" href="styleAlumno.css" title="styleSolicRes">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Solicitud para la residencia profesional</title>
 </head>
-<body>
+<body style="margin: 0;">
     <div class="container">
 		<div class="row">
 			<div class="left-column">
 				<a class="home-btn" href="a.html">
 					<h2><span style="margin-right: 10px;">Alumno</span></h2>
-					<img src="img/sombrero.png" width="50px">
+					<img src="../img/sombrero.png" width="50px">
 				</a>
 			</div>
 			<div class="center-column">
 				<h1>Solicitud de Residencia</h1>
 			</div>
 			<div class="right-column">
-				<a href="a.html"><img src="img/logout.png" width="40px"></a>
+				<a href="a.html"><img src="../img/logout.png" width="40px"></a>
 			</div>
 		</div>
 		<div class="button-row">
