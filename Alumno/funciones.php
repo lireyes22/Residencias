7a83574266c0d259032e36da72b6e32700dd2e94
Daniel Hernández Palomar <dh072878@gmail.com>
2023-05-02 18:57:16 -0500

    fdfd
    dfdf

---
+++ Alumno/funciones.php
@@ -0,0 +1,25 @@
+<?php
+function conn(){
+    $host = 'mapachitos.cisuktad1m53.us-east-2.rds.amazonaws.com';
+    $user = 'admin';
+    $password = 'mapachitos123';
+    $db = 'Residencias';
+    $conection = @mysqli_connect($host, $user, $password, $db);
+
+    if(!$conection){
+        echo 'Error de conexion';
+        return null;
+    }
+    mysqli_set_charset($conection, "utf8");
+    return $conection;
+}
+
+
+
+function Ejemplo() {
+    $conection = conn();
+    $consulta = "select xd............";
+    $query = mysqli_query($conection, $consulta);
+    return $query;
+}
+?>
