74aeee5611151f7a8289b5897bee21e08cecf1f2
lireyes22 <reyesvarguezli@gmail.com>
2023-05-05 13:35:16 -0500

    aa

---
+++ Asesor/AsesorInternoEvaluacionReporte.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Mi sitio web</title>
+    <link rel="stylesheet" href="../style/StyleBase.css">
+    <link rel="stylesheet" href="Style/StyleAsesor.css">
+    <meta charset="utf-8">
+</head>
+
+<body style="margin: 0;">
+    <div class="container">
+        <div class="row">
+            <div class="left-column">
+                <div class="dropdown">
+                    <a class="dropbtn home-btn" href="IndexAsesorInterno.html" style="text-decoration: none;"><span>Asesor</span><img src="img/asesor.png" width="50px"></a>
+                    <ul class="dropdown-content">
+                        <li><a href="../Profesor/indexProfesor.php"><span>Profesor</span><img src="img/profesor.png" width="50px"></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="center-column">
+                <h1>Evaluacion de Reporte</h1>
+            </div>
+            <div class="right-column">
+                <a href="a.html"><img src="img/logout.png" width="40px"></a>
+            </div>
+        </div>
+        <div class="button-row">
+            <a href="a.html" class="button-link">Asesorias</a>
+            <a href="a.html" class="button-link">Alumnos</a>
+            <a href="a.html" class="button-link">Reporte Semestral</a>
+            <a href="a.html" class="button-link">Residencias</a>
+        </div>
+    </div>
+    <div class="containerEv">
+            <div class="column-Ev1">
+                <label for="">Numero de control:</label>
+                <input type="text" name="" id="">
+            </div>
+            <div class="column-Ev2">Columna 2</div>
+            <div class="column-Ev3">Columna 3</div>
+    </div>
+</body>
+
+</html>
