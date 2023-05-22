3e250a01b96295b6f8fb743e3eff70959d72d059
Ferchoz1 <131812502+Ferchoz1@users.noreply.github.com>
2023-05-21 20:41:56 -0500

    new

--- DeptoAcademico/deptoAcaAsigProyRes.php
+++ DeptoAcademico/deptoAcaAsigProyRes.php
@@ -27,6 +27,7 @@
 				<h1>Asignación de Solicitud de proyecto</h1>
 			</div>
 			<div class="right-column">
+				<a href="config.php"><img src="../img/configuraciones.png" width="50px"></a> &nbsp; &nbsp;
 				<a href="../logout.php"><img src="../img/logout.png" width="40px"></a>
 			</div>
 		</div>
