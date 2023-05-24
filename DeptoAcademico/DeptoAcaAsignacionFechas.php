39ddb6f1e3c8e29d4925aa754f322d7271c61578
Ferchoz1 <131812502+Ferchoz1@users.noreply.github.com>
2023-05-24 14:01:43 -0500

    new

--- DeptoAcademico/DeptoAcaAsignacionFechas.php
+++ DeptoAcademico/DeptoAcaAsignacionFechas.php
@@ -25,7 +25,7 @@
 				<h1>Lista de Proyectos</h1>
 			</div>
 			<div class="right-column">
-				<a href="config.php"><img src="../img/configuraciones.png" width="50px"></a> &nbsp; &nbsp;
+				<a href="../usuariosConfig.php?idUsuario=<?php echo $_SESSION['id'];?>"><img src="../img/configuraciones.png" width="50px"></a> &nbsp; &nbsp;
 				<a href="../logout.php"><img src="../img/logout.png" width="40px"></a>
 			</div>
 		</div>
