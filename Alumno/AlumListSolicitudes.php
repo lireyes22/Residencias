d702edea6239921226aef55eb680e057aaff13ab
Ferchoz1 <131812502+Ferchoz1@users.noreply.github.com>
2023-05-24 16:13:45 -0500

    new

--- Alumno/AlumListSolicitudes.php
+++ Alumno/AlumListSolicitudes.php
@@ -30,7 +30,7 @@
 				<h2>Mis Solicitudes de Residencia</h2>
 			</div>
 			<div class="right-column">
-                <a href="config.php"><img src="../img/configuraciones.png" width="50px"></a> &nbsp; &nbsp;
+                <a href="../usuariosConfig.php?idUsuario=<?php echo $_SESSION['id'];?>"><img src="../img/configuraciones.png" width="50px"></a> &nbsp; &nbsp;
 				<a href="../logout.php"><img src="../img/logout.png" width="40px"></a>
 			</div>
 		</div>
