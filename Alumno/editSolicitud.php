c349cf0f5bd5c3cbf2948a88070330206c96dedb
Haziel18 <131190865+Haziel18@users.noreply.github.com>
2023-06-03 00:55:23 -0500

    poner nueva conexion de BD
    Alumno

--- Alumno/editSolicitud.php
+++ Alumno/editSolicitud.php
@@ -4,20 +4,8 @@ $residente = $_POST['residente'];
 $anteproyecto = $_FILES['anteproyecto']['tmp_name'];
 $antearchivo = file_get_contents($anteproyecto);
 $antearchivo = base64_encode($antearchivo);
-function conn(){
-    $host = 'mapachitos.cisuktad1m53.us-east-2.rds.amazonaws.com';
-    $user = 'admin';
-    $password = 'mapachitos123';
-    $db = 'Residencias';
-    $conection = @mysqli_connect($host, $user, $password, $db);
 
-    if(!$conection){
-        echo 'Error de conexion';
-        return null;
-    }
-    mysqli_set_charset($conection, "utf8");
-    return $conection;
-}
+include ('conectionBD.php');
 
 $conection=conn();
 //BUSCAR LA SOLICITUD DEL RESIDENTE
