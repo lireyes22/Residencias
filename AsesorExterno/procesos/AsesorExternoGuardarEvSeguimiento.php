5a26fb8e3ae6ae9b9b82da2e2616d50a76c19109
lireyes22 <reyesvarguezli@gmail.com>
2023-06-05 02:15:33 -0500

    a

--- AsesorExterno/procesos/AsesorExternoGuardarEvSeguimiento.php
+++ AsesorExterno/procesos/AsesorExternoGuardarEvSeguimiento.php
@@ -1,19 +1,5 @@
 <?php
-    /*function conn(){
-        $host = 'mapachitos.cisuktad1m53.us-east-2.rds.amazonaws.com';
-        $user = 'admin';
-        $password = 'mapachitos123';
-        $db = 'Residencias';
-        $conection = @mysqli_connect($host, $user, $password, $db);
-
-        if(!$conection){
-            echo 'Error de conexion';
-            return null;
-        }
-        mysqli_set_charset($conection, "utf8");
-        return $conection;
-    }*/
-    include ('../conectionBD.php');
+    include ('../../conectionBD.php');
 
     if(isset($_POST['Par1'])) {
         $nParcial = 1;
