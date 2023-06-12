31429dc635417d03e38c0d652410fc6f11f23a5c
lireyes22 <reyesvarguezli@gmail.com>
2023-06-12 02:35:59 -0500

    Update generador.php

--- Alumno/docs/generador.php
+++ Alumno/docs/generador.php
@@ -73,20 +73,7 @@
   //$anio = $a1->format('Y');
   //date_format($date, 'Y-m-d H:i:s');
   //OBTENER FIRMA
-  function conn(){
-    $host = 'mapachitos.cisuktad1m53.us-east-2.rds.amazonaws.com';
-    $user = 'admin';
-    $password = 'mapachitos123';
-    $db = 'Residencias';
-    $conection = @mysqli_connect($host, $user, $password, $db);
-
-    if(!$conection){
-        echo 'Error de conexion';
-        return null;
-    }
-    mysqli_set_charset($conection, "utf8");
-    return $conection;
-}
+  include_once("../conectionBD.php");
   $conn = conn();
   $sql = "SELECT Ufirma FROM Usuarios WHERE UID = $id"; 
   $result = mysqli_fetch_array(mysqli_query($conn, $sql));
