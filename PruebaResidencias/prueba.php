d8fb2d89cf3c066796327ae9fa365c34274b1718
TheJABS-Web <aldebaran1370@gmail.com>
2023-10-21 02:53:31 -0500

    LiBlack

---
+++ PruebaResidencias/prueba.php
@@ -0,0 +1,44 @@
+<?php
+include 'headprofesores.php';
+?>
+        <!-- Contenido principal -->
+        <div class="col ms-sm-auto px-4">
+				    <div class="container-fluid mt-3">
+				        <h2>Banco de proyectos</h2>
+				        <div class="table-responsive">
+				        	<table id="example" class="display table-striped table-hover" style="width:100%; background-color: #ededed;">
+						        <thead>
+						            <tr>
+						                <th>Nombre del proyecto</th>
+						                <th>Objetivo del proyecto</th>
+						                <th>Descripción</th>
+						                <th>Participantes</th>
+						                <th>Start date</th>						                
+						            </tr>
+						        </thead>
+						        <tbody>
+						        	<tr>
+						                <td>Tiger Nixon</td>
+						                <td>System Architect</td>
+						                <td>Edinburgh</td>
+						                <td>61</td>
+						                <td>2011-04-25</td>
+						            </tr>
+						        </tbody>
+						        <tfoot>
+						            <tr>
+						                <th>Nombre del proyecto</th>
+						                <th>Objetivo del proyecto</th>
+						                <th>Descripción</th>
+						                <th>Participantes</th>
+						                <th>Start date</th>						                
+						            </tr>
+						        </tfoot>
+						    </table>
+				        </div>           
+				        
+				    </div>
+				</div>
+<?php
+include 'footprofesores.php';
+?>
