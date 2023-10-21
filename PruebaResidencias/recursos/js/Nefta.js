001e48bb594c13b4d44a1b0f4c56bd9f55634bd5
Nefta56 <alexandro2856@gmail.com>
2023-10-20 20:28:49 -0500

    bootstrap

---
+++ PruebaResidencias/recursos/js/Nefta.js
@@ -0,0 +1,5 @@
+new DataTable('#example', {
+    paging: true, // Habilita la paginación
+    lengthChange: false, // Deshabilita la selección del número de elementos por página
+    pageLength: 15, // Establece el número de elementos por página
+});
