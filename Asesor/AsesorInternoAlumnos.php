0d42b3064ea8715dac7c5ec54fee7f9258864428
lireyes22 <reyesvarguezli@gmail.com>
2023-10-28 15:08:27 -0500

    AsesorInternoAlumnos

--- Asesor/AsesorInternoAlumnos.php
+++ Asesor/AsesorInternoAlumnos.php
@@ -1,55 +1,62 @@
 <?php 
 	include '../InicioSessionSeg.php';
     include ('funcAsesor.php');	
+    include 'headAsesorInterno.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<?php include ('encabezado.php'); encabezadox('Lista Alumnos') #encabezado xd?>
-
-    <div class="containerSRP">
-        <table>
-            <thead>
-                <tr>
-                    <th>Nombre del proyecto</th>
-                    <th>Número de control</th>
-                    <th>Nombre Completo</th>
-                    <th>Semestre Actual</th>
-                    <th>Correo Institucional</th>
-                    <th>Evaluación de Seguimiento</th>
-                    <th>Evaluación de Reporte Final</th>
-                </tr>
-            </thead>
-            <?php 
-                $query = consultaAsesorAlumno($_SESSION['id']);
-                $idAsesor = $_SESSION['id'];
-                //echo $idAsesor.'<br>';
-                //echo $_SESSION['id'];
-                //$idAsesor = 14;
-                //$query = consultaAsesorAlumno(14);
-                while($consulta = mysqli_fetch_array($query)){
-                    $idAlumno = $consulta['UAlumno'];
-                    $ProyectoS = ObtenerSolicitudProyecto($consulta['SPID']);
-                    $queryInfoAlumno = consultaUsuarioAlumno($idAlumno);
-                    $consultaAlumno = mysqli_fetch_array($queryInfoAlumno);
-                    
-
-            ?>
-            <form method="POST">
-            <tbody>
-                <td><?php echo $ProyectoS['SPNombreProyecto']?></td>
-                <td><?php echo $consultaAlumno['NumeroControl']?></td>
-                <td><?php echo $consultaAlumno['NombreCompleto']?></td>
-                <td><?php echo $consultaAlumno['SemestreActual']?></td>
-                <td><?php echo $consultaAlumno['CorreoInstitucional']?></td>
-                <td><input type="submit" formaction="AsesorInternoEvaluacionSeguimiento.php" value="Evaluación de Seguimiento" class="btn btn-actualizar"></td>
-                <td><input type="submit" formaction="AsesorInternoEvaluacionReporte.php" value="Evaluación de Reporte Final" class="btn btn-actualizar"></td>
-            </tbody>
-                    <input type="hidden" name="idAlumno" value="<?php echo $idAlumno; ?>">
-                    <input type="hidden" name="idAsesor" value="<?php echo $idAsesor; ?>">
-            </form>
-            <?php }//fin del while ?>
-        </table>
+    <!-- Contenido principal -->
+    <div class="col ms-sm-auto px-4">
+        <div class="container-fluid mt-3">
+            <h2>Residentes</h2>
+            <div class="table-responsive">
+                <table id="example" class="display table-striped table-hover" style="width:100%; background-color: #ededed;">
+                    <thead>
+                        <tr>
+                            <th>Nombre del proyecto</th>
+                            <th>Número de control</th>
+                            <th>Nombre completo</th>
+                            <th>Semestre actual</th>
+                            <th>Seguimiento</th>
+                            <th>Correo Institucional</th>
+                            <th> Reporte final </th>
+                        </tr>
+                    </thead>
+                    <?php 
+                        $query = consultaAsesorAlumno($_SESSION['id']);//Consulta datos de los alumnos asesorados
+                        $idAsesor = $_SESSION['id'];//Se guarda el id
+                        while($consulta = mysqli_fetch_array($query)){
+                            $idAlumno = $consulta['UAlumno'];
+                            $ProyectoS = ObtenerSolicitudProyecto($consulta['SPID']);
+                            $queryInfoAlumno = consultaUsuarioAlumno($idAlumno);
+                            $consultaAlumno = mysqli_fetch_array($queryInfoAlumno);  
+                    ?>
+                    <form method="POST">
+                    <tbody>
+                        <tr>
+                            <td><?php echo $ProyectoS['SPNombreProyecto']?></td>
+                            <td><?php echo $consultaAlumno['NumeroControl']?></td>
+                            <td><?php echo $consultaAlumno['NombreCompleto']?></td>
+                            <td><?php echo $consultaAlumno['SemestreActual']?></td>
+                            <td><?php echo $consultaAlumno['CorreoInstitucional']?></td>
+                            <td><input type="submit" formaction="AsesorInternoEvaluacionSeguimiento.php" value="Evaluación de Seguimiento" class="btn btn-primary"></td>
+                            <td><input type="submit" formaction="AsesorInternoEvaluacionReporte.php" value="Evaluación de Reporte Final" class="btn btn-primary"></td>
+                        </tr>
+                    </tbody>
+                        <input type="hidden" name="idAlumno" value="<?php echo $idAlumno; ?>">
+                        <input type="hidden" name="idAsesor" value="<?php echo $idAsesor; ?>">
+                        </form>
+                    <?php }//fin del while ?>
+                    <!-- <tfoot>
+                        <tr>
+                            <th>Nombre del proyecto</th>
+                            <th>Objetivo del proyecto</th>
+                            <th>Descripción</th>
+                            <th>Participantes</th>
+                            <th>Start date</th>
+                            <th>Detalles Proyectos</th>
+                        </tr>
+                    </tfoot> -->
+                </table>
+            </div>           
+        </div>
     </div>
-</body>
-</html>
+<?php include 'footer.php'; ?>
