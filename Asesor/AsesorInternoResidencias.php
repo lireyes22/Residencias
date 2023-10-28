7da80c8877f6022a505e7230b34e0d385a46cd6c
lireyes22 <reyesvarguezli@gmail.com>
2023-10-28 18:47:40 -0500

    AsesorInternoResidencias

--- Asesor/AsesorInternoResidencias.php
+++ Asesor/AsesorInternoResidencias.php
@@ -1,43 +1,82 @@
 <?php
-	include '../InicioSessionSeg.php';
-    include 'funcAsesor.php';
+include '../InicioSessionSeg.php';
+include('funcAsesor.php');
+include 'headAsesorInterno.php';
 ?>
-<!DOCTYPE html>
-<html>
-    
-    <?php include ('encabezado.php'); encabezadox('Lista Residencias') #encabezado xd?>
 
-	<div class="containerSRP">
-        <table>
-            <thead>
-                <tr>
-                    <th>Nombre del Proyecto</th>
-                    <th>Objetivo</th>
-                    <th>Descripcion</th>
-                    <th>Número de Residentes</th>
-                    <th>Actualizar Número de Residentes</th>
-                    <th>Detalles del Proyecto</th>
-                </tr>
-            </thead>
-            <?php
+<!-- Contenido principal -->
+<div class="col ms-sm-auto px-4">
+    <div class="container-fluid mt-3">
+        <h2>Proyectos Residencias</h2>
+        <div class="table-responsive">
+            <table id="example" class="display table-striped table-hover"
+                style="width:100%; background-color: #ededed;">
+                <thead>
+                    <tr>
+                        <th>Nombre del proyecto</th>
+                        <th>Objetivo del proyecto</th>
+                        <th>Descripción</th>
+                        <th>Número de Residentes</th>
+                        <th>Detalles Proyectos</th>
+                    </tr>
+                </thead>
+                <?php
                 $aiid = $_SESSION['AIID'];
                 $queryAR = consultaAsesorResidencias($aiid);
-                while($consultaAR = mysqli_fetch_array($queryAR)){
+                while ($consultaAR = mysqli_fetch_array($queryAR)) {
 
-            ?>
-            <form method="POST">
-                <tbody>
-                    <input type="hidden" name="SPID" value="<?php echo $consultaAR['SPID']; ?>">
-                    <td><?php echo $consultaAR['SPNombreProyecto']; ?></td>
-                    <td><?php echo $consultaAR['SPObjetivo']; ?></td>
-                    <td><?php echo $consultaAR['SPDescripcion']; ?></td>
-                    <td > <input type="number" min="<?php echo $consultaAR['SPEstudiantesRequeridos']; ?>" class="inp-tb" name="nResidentes" value="<?php echo $consultaAR['SPEstudiantesRequeridos']; ?>" required></td>
-                    <td><input type="submit" value="Actualizar" class="btn btn-actualizar" formaction="procesos/AsesorInternoActualizarResidencia.php"></td>
-                    <td><input type="submit" class="btn btn-actualizar" formaction="AsesorInternoDetallesProyectos.php" value="Detalles"></td>
-                </tbody>
-            </form>
-            <?php } ?>
-        </table>
+                    ?>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <?php echo $consultaAR['SPNombreProyecto']; ?>
+                            </td>
+                            <td>
+                                <?php echo $consultaAR['SPObjetivo']; ?>
+                            </td>
+                            <td>
+                                <?php echo $consultaAR['SPDescripcion']; ?>
+                            </td>
+                            <td>
+                                <form method="post">
+                                    <div class="input-group mb-3">
+                                        <input type="number" class="form-control" min="0" name="nResidentes"
+                                            value="<?php echo $consultaAR['SPEstudiantesRequeridos']; ?>" required>
+                                        <div class="input-group-append">
+                                            <input class="btn btn-outline-primary" type="submit" value="Actualizar"
+                                                formaction="procesos/AsesorInternoActualizarResidencia.php">
+                                        </div>
+                                    </div>
+                                    <input type="hidden" name="SPID" value="<?php echo $consultaAR['SPID']; ?>">
+                                </form>
+                            </td>
+                            <td>
+                                <div class="input-group mb-3">
+                                    <form method="post">
+                                        <input type="hidden" name="SPID" value="<?php echo $consultaAR['SPID']; ?>">
+                                        <input class="btn btn-primary" type="submit"
+                                            formaction="AsesorInternoDetallesProyectos.php" value="Detalles">
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                <?php } ?>
+                <!--
+                            <tfoot>
+                                <tr>
+                                    <th>Nombre del proyecto</th>
+                                    <th>Objetivo del proyecto</th>
+                                    <th>Descripción</th>
+                                    <th>Participantes</th>
+                                    <th>Start date</th>
+                                    <th>Detalles Proyectos</th>
+                                    
+                                </tr>
+                            </tfoot>
+                            -->
+            </table>
+        </div>
     </div>
-</body>
-</html>
+</div>
+<?php include 'footer.php'; ?>
---
+++ Asesor/antiguas/AsesorInternoResidenciasAntiguo.php
@@ -0,0 +1,43 @@
+<?php
+	include '../InicioSessionSeg.php';
+    include 'funcAsesor.php';
+?>
+<!DOCTYPE html>
+<html>
+    
+    <?php include ('encabezado.php'); encabezadox('Lista Residencias') #encabezado xd?>
+
+	<div class="containerSRP">
+        <table>
+            <thead>
+                <tr>
+                    <th>Nombre del Proyecto</th>
+                    <th>Objetivo</th>
+                    <th>Descripcion</th>
+                    <th>Número de Residentes</th>
+                    <th>Actualizar Número de Residentes</th>
+                    <th>Detalles del Proyecto</th>
+                </tr>
+            </thead>
+            <?php
+                $aiid = $_SESSION['AIID'];
+                $queryAR = consultaAsesorResidencias($aiid);
+                while($consultaAR = mysqli_fetch_array($queryAR)){
+
+            ?>
+            <form method="POST">
+                <tbody>
+                    <input type="hidden" name="SPID" value="<?php echo $consultaAR['SPID']; ?>">
+                    <td><?php echo $consultaAR['SPNombreProyecto']; ?></td>
+                    <td><?php echo $consultaAR['SPObjetivo']; ?></td>
+                    <td><?php echo $consultaAR['SPDescripcion']; ?></td>
+                    <td><input type="number" min="<?php echo $consultaAR['SPEstudiantesRequeridos']; ?>" class="inp-tb" name="nResidentes" value="<?php echo $consultaAR['SPEstudiantesRequeridos']; ?>" required></td>
+                    <td><input type="submit" value="Actualizar" class="btn btn-actualizar" formaction="procesos/AsesorInternoActualizarResidencia.php"></td>
+                    <td><input type="submit" class="btn btn-actualizar" formaction="AsesorInternoDetallesProyectos.php" value="Detalles"></td>
+                </tbody>
+            </form>
+            <?php } ?>
+        </table>
+    </div>
+</body>
+</html>
