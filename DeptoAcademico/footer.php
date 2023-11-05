5467770ee76236919cefcfcbaa37a5a9a4ecd590
TheJABS-Web <aldebaran1370@gmail.com>
2023-11-04 21:33:00 -0500

    MOYYYYY
    a

--- DeptoAcademico/footer.php
+++ DeptoAcademico/footer.php
@@ -8,12 +8,5 @@
 </div>
 
 
-
-
-<!--Scripts-->
-<script src="../recursos/js/jquery-3.6.0.min.js"></script>
-<script src="../recursos/js/bootstrap.bundle.min.js"></script>
-<script src="../recursos/js/jquery.dataTables.min.js"></script> 	
-<script src="../recursos/js/Nefta.js"></script>
 </body>
 </html>
