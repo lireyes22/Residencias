7324d77c1d16f6fcabbfb5e60dcf7d0f0e9ca851
TheJABS-Web <aldebaran1370@gmail.com>
2023-11-04 20:41:23 -0500

    faq

--- Alumno/headAlumnos.php
+++ Alumno/headAlumnos.php
@@ -125,7 +125,7 @@
                             </a>
                         </li>
                         <li id="opcion" class="nav-item p-3 row">
-                            <a href="#" class="d-flex">
+                            <a href="faq.php" class="d-flex">
                             <div class="col-3 centrar">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-question-circle" viewBox="0 0 16 16">
                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
