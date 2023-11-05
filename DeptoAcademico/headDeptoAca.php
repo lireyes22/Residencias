3da51b893582ca06061e829d95698a5bec96f641
TheJABS-Web <aldebaran1370@gmail.com>
2023-11-04 22:12:59 -0500

    moy 3

--- DeptoAcademico/headDeptoAca.php
+++ DeptoAcademico/headDeptoAca.php
@@ -9,10 +9,6 @@
     <link href="../recursos/css/jquery.dataTables.min.css" rel="stylesheet"> 
 
     <!--Scripts-->
-    <script src="../recursos/js/jquery-3.6.0.min.js"></script>
-    <script src="../recursos/js/bootstrap.bundle.min.js"></script>
-    <script src="../recursos/js/jquery.dataTables.min.js"></script> 	
-    <script src="../recursos/js/Nefta.js"></script>
 
 </head>
 <body> 
